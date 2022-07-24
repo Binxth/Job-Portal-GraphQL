053d2246ae1718b3de23dcd942b7f82135e7f5ee
Binxth <33088210+Binxth@users.noreply.github.com>
2022-07-25 01:03:36 +0530

    updated mutations

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,24 +1,36 @@
-const db = require('./db')
+const db = require("./db");
 
 const Query = {
-  jobs: () =>  db.jobs.list(),
-  job:(root, args)=> db.jobs.get(args.id),
-  company:(root,args)=>db.companies.get(args.id),
-  
+  jobs: () => db.jobs.list(),
+  job: (root, args) => db.jobs.get(args.id),
+  company: (root, args) => db.companies.get(args.id),
+};
+
+// const Mutation = {
+//   createJob : (root, {companyId, title, description})=>{
+//     const id= db.jobs.create({companyId, title, description});
+//     return db.jobs.get(id);
+//   }
+// };
+
+const Mutation = {
+  createJob: (root, { input }) => {
+    const id = db.jobs.create(input);
+    return db.jobs.get(id);
+  },
 };
 
 //as each Job has a Company object not a company id, has to resolve it as below
 //resolving fields of Job
 const Job = {
   //job is the parent object
-  company: (job)=> db.companies.get(job.companyId)
-}
+  company: (job) => db.companies.get(job.companyId),
+};
 
 const Company = {
-  jobs: (company) => db.jobs.list().filter(
-    (job)=> job.companyId === company.id
-  )
-}
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
 
-//export all resolvers
-module.exports = { Query, Job,Company };
+//export all 
+module.exports = { Query, Mutation, Job, Company };
